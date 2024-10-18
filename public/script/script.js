163b8d50f97d53cbb81ce70057d7a8ddc09199f3
Leonardo Martin <leonardomartin.dev@gmail.com>
2024-10-18 20:40:00 -0300

    layout do modal de edição das tasks e javascript

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,4 @@
 // Lógica para formatar a data atual na página
-
 const getFormattedDate = () => {
     const daysOfWeek = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
     const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
@@ -14,14 +13,26 @@ const getFormattedDate = () => {
 
 const formattedDate = getFormattedDate();
 
-const actualDate = document.getElementById("actual-date")
+const actualDate = document.getElementById("actual-date");
 actualDate.innerHTML = formattedDate;
 
-// Lógica para mostrar ou esconder as tarefas concluidas
-const containerTasksFinished = document.getElementById("container-tasks-finished")
-const arrowShow = document.querySelector(".fa-solid")
+// Lógica para mostrar ou esconder as tarefas concluídas
+const containerTasksFinished = document.getElementById("container-tasks-finished");
+const arrowShow = document.querySelector(".fa-solid");
 
-const showTasks = document.querySelector(".show-tasks").addEventListener("click", () =>{
-    containerTasksFinished.classList.toggle("hide")
-    arrowShow.classList.toggle("open")
-})
+document.querySelector(".show-tasks").addEventListener("click", () => {
+    containerTasksFinished.classList.toggle("hide");
+    arrowShow.classList.toggle("open");
+});
+
+// Lógica para fechar e mostrar modal
+const modal = document.querySelector(".modal-container");
+
+function closeModal(e) {
+    e.preventDefault()
+    modal.classList.remove("modal-container");
+    modal.classList.add("hide");
+}
+
+document.querySelector(".close-modal").addEventListener("click", closeModal);
+document.querySelector(".cancel-btn").addEventListener("click", closeModal);
