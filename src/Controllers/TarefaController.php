7f5276034894b3f3f0d7b094f2a89ed192eb153f
Leonardo Martin <leonardomartin.dev@gmail.com>
2024-10-19 12:10:44 -0300

    implementação do método gerarJson no Controller de salvar os dados em JSON na pasta data

--- src/Controllers/TarefaController.php
+++ src/Controllers/TarefaController.php
@@ -14,6 +14,7 @@ class TarefaController {
         require __DIR__ . '/../../views/listarTarefas.php';
     }
 
+
     public function criarTarefas($titulo, $descricao = null) { // Define $descricao como null por padrão
         $tarefa = new Tarefa();
         $tarefa->titulo = $titulo;
@@ -30,12 +31,14 @@ class TarefaController {
 
             if (!empty($titulo)) {
                 $this->criarTarefas($titulo, $descricao);
+                $this->gerarJson(); // Gera o arquivo JSON após adicionar
             }
 
             header('Location: /');
             exit();
         }
     }
+
 
 
     public function listarTarefa(){
@@ -48,12 +51,15 @@ class TarefaController {
         $tarefa->titulo = $titulo;
         $tarefa->descricao = $descricao;
         $tarefa->concluida = $concluida;
-        return $this->repository->update($tarefa);
+        $this->repository->update($tarefa);
+        $this->gerarJson(); // Gera o arquivo JSON após editar
     }
 
     public function deletarTarefa($id){
-        return $this->repository->deletarTarefa($id);
+        $this->repository->deletarTarefa($id);
+        $this->gerarJson(); // Gera o arquivo JSON após deletar
     }
+
 
     public function marcarTarefaConcluida($id) {
         return $this->repository->concluirTarefa($id);
@@ -62,6 +68,17 @@ class TarefaController {
     public function pesquisarTarefas($titulo) {
         return $this->repository->pesquisarPorTitulo("%$titulo%");
     }
+
+    public function gerarJson() {
+        $tarefas = $this->repository->getAll(); // Obtém todas as tarefas
+        $json = json_encode($tarefas, JSON_PRETTY_PRINT); // Converte para JSON
+
+        $filePath = __DIR__ . '/../../data/tarefas.json';
+
+        // Salva o JSON no arquivo
+        file_put_contents($filePath, $json);
+    }
+
 
 
 }
